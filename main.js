a574fe78e1f7da38abe0fbec44e617b69976360b
battistaennio <ennio.battista99@gmail.com>
2024-06-06 16:36:51 +0200

    ESERCIZIO TERMINATO

--- main.js
+++ main.js
@@ -16,34 +16,23 @@ for (let UnoCento = 1; UnoCento <= 100; UnoCento++) {
     //creazione div
     let box = document.createElement("div"); 
 
-    //aggiungere contenuto al box (div) tramite criteri
+    //aggiungere contenuto e classe al box (div) secondo criteri
     if (UnoCento % 5 === 0 && UnoCento % 3 === 0) { //divisibile sia per 5 che per 3
         box.append("FizzBuzz");
-    
-    } else if (UnoCento % 5 === 0) { //divisibile per 5
-        box.append("Buzz");
-    
-    } else if (UnoCento % 3 === 0) { //divisibile per 3
-        box.append("Fizz");
-    
-    } else {
-        box.append(UnoCento);
-    }
-
-    //aggiungere classe al box (div) tramite criteri
-    if (UnoCento % 5 === 0 && UnoCento % 3 === 0) { //divisibile sia per 5 che per 3
         box.classList.add("fizzbuzz");
     
     } else if (UnoCento % 5 === 0) { //divisibile per 5
+        box.append("Buzz");
         box.classList.add("buzz");
     
     } else if (UnoCento % 3 === 0) { //divisibile per 3
+        box.append("Fizz");
         box.classList.add("fizz");
     
     } else {
+        box.append(UnoCento);
         box.classList.add("box");
     }
-
 
     //aggiungere i box (div) al container
     container.append(box)    
