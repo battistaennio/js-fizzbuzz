9e2d30c9d45265530ce207fff74dc6532fdd92e7
battistaennio <ennio.battista99@gmail.com>
2024-06-06 16:23:04 +0200

    Creata logica per aggiungere classe al box secondo i criteri

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 //Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 
+//Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
+
+
+//Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
+
+
 //selettore div.container
 const container = document.querySelector(".container");
 
@@ -24,10 +30,21 @@ for (let UnoCento = 1; UnoCento <= 100; UnoCento++) {
         box.append(UnoCento);
     }
 
+    //aggiungere classe al box (div) tramite criteri
+    if (UnoCento % 5 === 0 && UnoCento % 3 === 0) { //divisibile sia per 5 che per 3
+        box.classList.add("fizzbuzz");
+    
+    } else if (UnoCento % 5 === 0) { //divisibile per 5
+        box.classList.add("buzz");
+    
+    } else if (UnoCento % 3 === 0) { //divisibile per 3
+        box.classList.add("fizz");
+    
+    } else {
+        box.classList.add("box");
+    }
+
 
     //aggiungere i box (div) al container
     container.append(box)    
 }
-
-
-//Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
