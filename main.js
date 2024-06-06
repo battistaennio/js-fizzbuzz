e124cd958aeae26f142c7e241d1357f4f86afbed
battistaennio <ennio.battista99@gmail.com>
2024-06-06 15:10:35 +0200

    Creato div.container e integrata logica per creare le box con all'interno i numeri

--- main.js
+++ main.js
@@ -1,21 +1,32 @@
 //Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
+
+//selettore div.container
+const container = document.querySelector(".container");
+
 // creazione ciclo per stampare numeri da 1 a 100
 for (let UnoCento = 1; UnoCento <= 100; UnoCento++) {
+    
+    //creazione div.box
+    let box = `<div class="box">${UnoCento}</div>`; 
 
     //conzdizione per sostituire al numero il fizzbuzz/buzz/fizz
     if (UnoCento % 5 === 0 && UnoCento % 3 === 0) { //divisibile sia per 5 che per 3
-        console.log("FizzBuzz");
+        box = `<div class="box">FizzBuzz</div>`;
     
     } else if (UnoCento % 5 === 0) { //divisibile per 5
-        console.log("Buzz");
+        box = `<div class="box">Buzz</div>`;
     
     } else if (UnoCento % 3 === 0) { //divisibile per 3
-        console.log("Fizz");
+        box = `<div class="box">Fizz</div>`;
     
     } else {
-        console.log(UnoCento)
+        box;
     }
+
+    //le box vengono inserite nel container
+    container.innerHTML += box;
+    console.log(box);
     
 }
 
