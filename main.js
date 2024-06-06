5866b422ccb94d0c2f5117270cc1faf93ba49d3c
battistaennio <ennio.battista99@gmail.com>
2024-06-06 14:36:43 +0200

    Creazione ciclo per stampare numeri da 1 a 100

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 //Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+// creazione ciclo per stampare numeri da 1 a 100
+for (let UnoCento = 1; UnoCento <= 100; UnoCento++) {
+    console.log(UnoCento)
+}
+
+
